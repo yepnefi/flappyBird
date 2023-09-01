4b27f9a0cfddddaf3f1a1213af5793f96670a561
Armin <92278038+yepnefi@users.noreply.github.com>
2023-09-01 08:57:25 +0200

    added bird

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
+    const canvas = document.getElementById('gamecanvas');
+    const context = canvas.getContext('2d');
 
-})
+    const bird = {
+        x: canvas.width / 100, // starting x position
+        y: canvas.height / 2.5, // starting y position
+        width: 20, // width of bird
+        height: 20, // height of bird
+        velocityY: 0,  // Vertical velocity
+        gravity: 0.3, // Gravity strength
+        jumpStrength: -4, // Strength of the jump
+    }
+
+    function gameloop(){
+        context.clearRect(0, 0, canvas.width, canvas.height); // Clear canvas
+
+        context.fillStyle = "blue";
+        context.fillRect(bird.x, bird.y, bird.width, bird.height);
+        requestAnimationFrame(gameloop); // Repeat the loop
+    }
+   gameloop();
+});
