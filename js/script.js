f964582fa6b83ff9d64bd64b00aa2f025e22bc2e
Armin <92278038+yepnefi@users.noreply.github.com>
2023-09-01 09:02:23 +0200

    draw bird + first jump check

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     const canvas = document.getElementById('gamecanvas');
     const context = canvas.getContext('2d');
+    let firstJump = false;
 
     const bird = {
         x: canvas.width / 100, // starting x position
@@ -8,16 +9,51 @@ document.addEventListener("DOMContentLoaded", () => {
         width: 20, // width of bird
         height: 20, // height of bird
         velocityY: 0,  // Vertical velocity
-        gravity: 0.3, // Gravity strength
+        gravity: 0.2, // Gravity strength
         jumpStrength: -4, // Strength of the jump
+    }
+
+    // jump when spacebar is hit
+    document.addEventListener("keydown", (event) => {
+        if (event.code === "Space") {
+            if (!firstJump) {
+                firstJump = true;
+            }
+            // on jump
+            bird.velocityY = bird.jumpStrength;
+        }
+    });
+
+    function updateBird() {
+        // falling animation for bird
+        bird.velocityY += bird.gravity;
+
+        // Apply gravity after first jump
+        if (firstJump) {
+            bird.y += bird.velocityY;
+        } else {
+            bird.y = canvas.height / 2.5; // Bird stays in the middle before the first jump
+        }
+        // prevent bird from falling below canvas
+        if (bird.y + bird.height > canvas.height) {
+            bird.y = canvas.height - bird.height;
+            bird.velocityY = 0;
+        }
+        // prevent bird from going above the canvas
+        if (bird.y < 0) {
+            bird.y = 0; // Set the bird's y-position to the top of the canvas
+            bird.velocityY = 0; // Stop the upward velocity
+        }
     }
 
     function gameloop(){
         context.clearRect(0, 0, canvas.width, canvas.height); // Clear canvas
 
-        context.fillStyle = "blue";
-        context.fillRect(bird.x, bird.y, bird.width, bird.height);
-        requestAnimationFrame(gameloop); // Repeat the loop
+        updateBird(); // draw new bird position
+
+        context.fillStyle = "blue"; // bird color
+        context.fillRect(bird.x, bird.y, bird.width, bird.height); // draw bird
+        requestAnimationFrame(gameloop); // adds animation for each frame + repeats the loop
     }
    gameloop();
 });
